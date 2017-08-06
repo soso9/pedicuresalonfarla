ed81e83b703b9c6746dc57602136f596f28d20e3
Thomas Farla <thomas-farla@hotmail.com>
2017-08-06 16:01:35 +0200

    Make header fixed

--- src/components/toolbar.js
+++ src/components/toolbar.js
@@ -11,7 +11,7 @@ class Toolbar extends React.Component {
 
   render() {
     return (
-      <header className="mdc-toolbar">
+      <header className="mdc-toolbar mdc-toolbar--fixed">
         <div className="mdc-toolbar__row">
         <section className="mdc-toolbar__section mdc-toolbar__section--align-start">
           <a href="#" className="material-icons mdc-toolbar__icon--menu">menu</a>
